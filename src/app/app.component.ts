aba5bed39bc9c1904e99528a060e5e0053e8d5ec
nagamadhan_1 <nagamadhan_1@gmail.com>
2018-04-25 17:31:29 +0530

    components & styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: blue;
+    }
+    p {
+      animation: ease-in-out;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'the World of Phantom !!!';
-  name = '';
+  //title = 'the World of Phantom !!!';
+  //name = '';
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //templateUrl: './servers.component.html',
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
+  templateUrl: './servers.component.html',
   //template: '<app-server></app-server><app-server></app-server>',
-  template: `
+  /*template: `
     <app-server></app-server>
     <app-server></app-server>
     <app-server></app-server>
-  `,
+  `,*/
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
